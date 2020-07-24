cb6de68e1d4275abd5f9f18278696a8f00d9c365
Martín Basterrechea <martinbaste@gmail.com>
2020-07-24 14:15:52 +0200

    long description

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
 from setuptools import setup, find_packages
 
+with open('README.md') as f:
+    long_description = f.read()
+
 setup(
     name='index2rc',
-    version='1.0.0',
+    version='1.0.1',
     description='Creates illumina samplesheet with the reverse complement of the second index (index2).',
+    long_description=long_description,
+    long_description_content_type='text/markdown',  # This is important!
     url='https://github.com/ssi-dk/index2rc',
     author="Martin Basterrechea",
     author_email="mbas@ssi.dk",
